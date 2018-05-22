17f6d9e71bbed528fa4fb74fcaa2bc5ad9ae88b5
Krystian Fras <krystian@fras.me>
2018-05-22 16:45:25 +0200

    working

---
+++ StockTickRApp/Startup.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using StockTickR.Hubs;
+
+namespace StockTickR
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+           services.AddCors(o => o.AddPolicy("CorsPolicy", builder => {
+               builder
+               .AllowAnyMethod()
+               .AllowAnyHeader()
+               .AllowCredentials()
+               .AllowAnyOrigin();
+            }));
+
+
+            services.AddSignalR()
+                    .AddMessagePackProtocol();
+
+            services.AddSingleton<StockTicker>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseFileServer();
+            app.UseCors("CorsPolicy");
+            app.UseSignalR(routes =>
+            {
+                routes.MapHub<StockTickerHub>("/stocks");
+            });
+        }
+    }
+}
