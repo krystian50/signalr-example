09c9c210e5147e215e5e1e3496587747a3c68e20
Krystian Fras <krystian@fras.me>
2018-05-29 21:53:44 +0200

    changed url

--- CsharpClient/Program.cs
+++ CsharpClient/Program.cs
@@ -13,7 +13,7 @@ namespace CsharpClient
         static async Task Main(string[] args)
         {
             var connection = new HubConnectionBuilder()
-                .WithUrl("http://localhost:5000/stocks")
+                .WithUrl("/stocks")
                 .ConfigureLogging(logging =>
                 {
                     logging.AddConsole();
